b6d2a27f9d92f1bb6b70e81ee4313a9b41417337
olhkyle <itskkyle@gmail.com>
2025-10-15 17:19:49 +0900

    chore : remove console.log

--- src/app/(service)/page.tsx
+++ src/app/(service)/page.tsx
@@ -4,7 +4,7 @@ import { createClient } from '@/utils/supabase/server';
 export default async function HomePage() {
 	const supabase = await createClient();
 	const { data } = await supabase.from('documents').select('*');
-	console.log(data);
+	// console.log(data);
 
 	return (
 		<div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
