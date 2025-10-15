fc0bf134cf2c99988c9c5a5d062517695054f767
olhkyle <itskkyle@gmail.com>
2025-10-15 17:10:21 +0900

    type : update `database.types.ts`

--- src/utils/supabase/database.types.ts
+++ src/utils/supabase/database.types.ts
@@ -3,37 +3,34 @@ export type Json = string | number | boolean | null | { [key: string]: Json | un
 export interface Database {
 	public: {
 		Tables: {
-			statement: {
+			documents: {
 				Row: {
 					// the data expected from .select()
 					id: string;
 					user_id: string;
-					title: string;
-					writer: string;
-					total_amount: number;
+					name: string;
+					pages: number;
 					created_at: string;
 					updated_at: string;
 				};
 				Insert: {
 					id: string;
 					user_id: string;
-					title: string;
-					writer: string;
-					total_amount: number;
+					name: string;
+					pages: number;
 					created_at: string;
 					updated_at: string;
 				};
 				Update: {
 					id: string;
 					user_id: string;
-					title: string;
-					writer: string;
-					total_amount: number;
+					name: string;
+					pages: number;
 					updated_at: string;
 				};
 				Delete: {
 					id: never;
-					user_id: never;
+					user_id: string;
 				};
 			};
 		};
