d02c7a873dd6c3ec8d955245638bb3d4d30632bf
olhkyle <itskkyle@gmail.com>
2025-10-15 17:11:57 +0900

    page : remove unused pages

--- src/app/(service)/from-excel/page.tsx
+++
@@ -1,3 +0,0 @@
-export default async function FromExcelImportPage() {
-	return <>FromExcelImportPage</>;
-}
--- src/app/(service)/statements/page.tsx
+++
@@ -1,3 +0,0 @@
-export default async function StatementsPage() {
-	return <>StatementsPage</>;
-}
