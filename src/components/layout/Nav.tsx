6b5463be35a1728b08c9ca50ef8fcdd3d3dd061e
olhkyle <itskkyle@gmail.com>
2025-10-15 17:10:52 +0900

    feat : change title of project

--- src/components/layout/Nav.tsx
+++ src/components/layout/Nav.tsx
@@ -1,7 +1,7 @@
 const Nav = () => {
 	return (
 		<nav className="bg-muted border-subtle sticky top-0 z-40 flex w-full items-center justify-between border-b bg-opacity-50 px-4 py-1.5 backdrop-blur-lg sm:p-4 md:hidden">
-			ACTX
+			ACPX
 		</nav>
 	);
 };
