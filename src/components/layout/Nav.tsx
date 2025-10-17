1a5b1020f443ee03121109a34929ff99797cda07
olhkyle <kylekwon.dev@gmail.com>
2025-10-17 23:35:44 +0900

    constant : add Link func on Logo

--- src/components/layout/Nav.tsx
+++ src/components/layout/Nav.tsx
@@ -1,11 +1,15 @@
+import Link from 'next/link';
 import { Menu } from 'lucide-react';
 import { Button } from '../ui';
+import route from '@/constant/route';
 
 export default function Nav() {
 	return (
 		<nav className="fixed top-4 left-[50%] flex min-h-[60px] p-4 w-[calc(100dvw-32px)] items-center justify-between border-[1px]  border-gray-200 bg-white rounded-full backdrop-blur-lg z-40 translate-x-[-50%] md:hidden">
 			<div className="flex justify-between items-center flex-1">
-				<h1 className="text-xl font-black">SKEW</h1>
+				<h1 className="text-xl font-black">
+					<Link href={route.SERVICE.ROOT}>SKEVV</Link>
+				</h1>
 				<Button type="button" variant="secondary">
 					<Menu />
 				</Button>
