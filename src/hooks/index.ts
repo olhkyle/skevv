04a3e15640fbc0803892d01b94c375c2fcc6e008
olhkyle <itskkyle@gmail.com>
2025-10-28 15:51:15 +0900

    hooks : develop `useResizableObserver` hook
    - react with OS level screen resizing

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,2 +1,4 @@
 export { default as useIsMountedRef } from './useIsMountedRef';
 export { default as useLoading } from './useLoading';
+export { default as useMediaQuery } from './useMediaQuery';
+export { default as useResizableObserver } from './useResizableObserver';
---
+++ src/hooks/useResizableObserver.ts
@@ -0,0 +1,53 @@
+import React from 'react';
+
+interface UseResizableObserverProps {
+	initialWidth?: number;
+	effectTriggers: (number | string | boolean)[];
+}
+
+export default function useResizableObserver<T extends HTMLElement>({ initialWidth = 0, effectTriggers = [] }: UseResizableObserverProps) {
+	const [containerWidth, setContainerWidth] = React.useState<number>(initialWidth);
+
+	const containerRef = React.useRef<T>(null);
+
+	/**
+	 * ⚡️ Change width depends on parent width
+	 * box-sizing: border-box -> width: content + padding + border
+	 * 
+	 * offsetWidth - content + padding + border	px (number)	정수
+		 clientWidth - content + padding	px (number)	정수
+		 scrollWidth - content + padding + 스크롤 영역 포함	px (number)	정수
+		 getComputedStyle(element).width - CSS상 width (box-sizing 영향 있음) "500px" (문자열) 소수점 가능
+	 */
+
+	const handleResize = () => {
+		if (containerRef.current) {
+			const style = getComputedStyle(containerRef.current);
+			const paddingLeft = parseFloat(style.paddingLeft) || 0;
+			const paddingRight = parseFloat(style.paddingRight) || 0;
+			const width = containerRef.current.offsetWidth - (paddingLeft + paddingRight);
+
+			setContainerWidth(width);
+		}
+	};
+
+	React.useEffect(() => {
+		if (!containerRef.current) return;
+
+		handleResize();
+
+		const observer = new ResizeObserver(() => {
+			handleResize();
+		});
+
+		observer.observe(containerRef.current);
+		window.addEventListener('resize', handleResize);
+
+		return () => {
+			observer.disconnect();
+			window.removeEventListener('resize', handleResize);
+		};
+	}, [...effectTriggers]);
+
+	return { containerRef, containerWidth };
+}
