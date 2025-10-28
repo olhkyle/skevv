b96af8c3093bea99d66d08afbfe6953166d7c51c
olhkyle <itskkyle@gmail.com>
2025-10-28 16:35:58 +0900

    fix : fix `useResizableObserver` hook with blinking issue

--- src/hooks/useResizableObserver.ts
+++ src/hooks/useResizableObserver.ts
@@ -19,36 +19,52 @@ export default function useResizableObserver<T extends HTMLElement>({ initialWid
 		 scrollWidth - content + padding + 스크롤 영역 포함	px (number)	정수
 		 getComputedStyle(element).width - CSS상 width (box-sizing 영향 있음) "500px" (문자열) 소수점 가능
 	 */
+	const setWidthSafely = (nextWidth: number) => {
+		const roundedWidth = Math.round(nextWidth);
 
-	const handleResize = () => {
-		if (containerRef.current) {
-			const style = getComputedStyle(containerRef.current);
-			const paddingLeft = parseFloat(style.paddingLeft) || 0;
-			const paddingRight = parseFloat(style.paddingRight) || 0;
-			const width = containerRef.current.offsetWidth - (paddingLeft + paddingRight);
+		// 1px 오차
+		setContainerWidth(prevWidth => {
+			if (Math.abs(prevWidth - roundedWidth) >= 1) return roundedWidth;
 
-			setContainerWidth(prevWidth => {
-				if (Math.abs(prevWidth - width) > 0.5) return width; // 0.5px 오차 허용
-				return prevWidth;
-			});
-		}
+			return prevWidth;
+		});
 	};
+
+	// const handleResize = () => {
+	// 	if (containerRef.current) {
+	// 		const style = getComputedStyle(containerRef.current);
+	// 		const paddingLeft = parseFloat(style.paddingLeft) || 0;
+	// 		const paddingRight = parseFloat(style.paddingRight) || 0;
+	// 		const width = containerRef.current.offsetWidth - (paddingLeft + paddingRight);
+
+	// 		setContainerWidth(prevWidth => {
+	// 			if (Math.abs(prevWidth - width) >= 1) return width; // 0.5px 오차 허용
+	// 			return prevWidth;
+	// 		});
+	// 	}
+	// };
 
 	React.useEffect(() => {
 		if (!containerRef.current) return;
 
-		handleResize();
+		setWidthSafely(containerRef.current.getBoundingClientRect().width);
 
-		const observer = new ResizeObserver(() => {
-			handleResize();
+		const style = getComputedStyle(containerRef.current);
+		const paddingLeft = parseFloat(style.paddingLeft) || 0;
+		const paddingRight = parseFloat(style.paddingRight) || 0;
+
+		const observer = new ResizeObserver((entries: ResizeObserverEntry[]) => {
+			const [entry] = entries;
+
+			const width = entry.contentRect?.width ?? containerRef.current!.offsetWidth - (paddingLeft + paddingRight);
+
+			setWidthSafely(width);
 		});
 
-		observer.observe(containerRef.current);
-		// window.addEventListener('resize', handleResize);
+		observer.observe(containerRef.current, { box: 'border-box' });
 
 		return () => {
 			observer.disconnect();
-			// window.removeEventListener('resize', handleResize);
 		};
 	}, [...effectTriggers]);
 
