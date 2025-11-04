4e8ec4f4eb1789fba0848356a159167fea2f5f49
olhkyle <itskkyle@gmail.com>
2025-11-04 17:17:26 +0900

    style : change size of `Loader`

--- src/components/common/LayoutSpinner.tsx
+++ src/components/common/LayoutSpinner.tsx
@@ -3,7 +3,7 @@ import { Loader } from 'lucide-react';
 export default function LayoutSpinner() {
 	return (
 		<div className="flex justify-center items-center flex-1 w-full h-screen">
-			<Loader className="animate-spin" />
+			<Loader className="animate-spin" size={18} />
 		</div>
 	);
 }
