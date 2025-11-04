3db3f21dc135392ee193888569e5e08b7c56b5a6
olhkyle <itskkyle@gmail.com>
2025-11-04 11:54:13 +0900

    hooks : add screenSize explanation

--- src/hooks/useMediaQuery.ts
+++ src/hooks/useMediaQuery.ts
@@ -1,5 +1,16 @@
 import React from 'react';
 
+/**
+ * query (screenSize)
+ * - max_[SCREEN_SIZE] - is less than SCREEN_SIZE
+ * - min_[SCREEN_SIZE] - is bigger than SCREEN_SIZE
+ * 1. XS : 480px
+ * 2. XM : 640px
+ * 3. MD : 768px
+ * 4. LG : 1024px
+ * 5. XL : 1280px
+ * 6. 2XL : 1536px
+ */
 export default function useMediaQuery(query: string) {
 	const [matches, setMatches] = React.useState(() => {
 		if (typeof window !== 'undefined') {
