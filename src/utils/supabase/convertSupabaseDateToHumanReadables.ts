11e42a28242eab960ae0966967255e0554e71582
olhkyle <itskkyle@gmail.com>
2025-11-13 18:22:41 +0900

    lint : remove @ts-ignore

--- src/utils/supabase/convertSupabaseDateToHumanReadables.ts
+++ src/utils/supabase/convertSupabaseDateToHumanReadables.ts
@@ -7,16 +7,16 @@ function convertSupabaseDateToHumanReadable(date: string) {
 		hour: 'numeric',
 		minute: 'numeric',
 		second: 'numeric',
-	};
-	// @ts-ignore
+	} as const;
+
 	const formattedDate = new Intl.DateTimeFormat('en-US', options).format(parsedDate);
 	return formattedDate;
 }
 
 function convertSupabaseDateToShortHumanReadable(date: string) {
 	const parsedDate = new Date(date);
-	const options = { year: 'numeric', month: 'numeric', day: 'numeric' };
-	// @ts-ignore
+	const options = { year: 'numeric', month: 'numeric', day: 'numeric' } as const;
+
 	const formattedDate = new Intl.DateTimeFormat('en-US', options).format(parsedDate);
 	return formattedDate;
 }
