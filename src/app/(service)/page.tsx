d547c02ee90423a9f7d40d024421921fc5426952
olhkyle <itskkyle@gmail.com>
2025-11-18 17:19:01 +0900

    style : change padding on HomePage

--- src/app/(service)/page.tsx
+++ src/app/(service)/page.tsx
@@ -10,7 +10,7 @@ export default async function HomePage() {
 	// console.log(data);
 
 	return (
-		<div className="flex flex-col justify-items-center min-h-screen gap-3 p-8 pb-20 bg-white sm:p-16">
+		<div className="flex flex-col justify-items-center min-h-screen gap-3 p-3 pb-20 bg-white">
 			<div className="p-12 text-white font-semibold bg-gradient-gray-100 rounded-lg">
 				<p>This is a lightweight PDF Editor</p>
 				<p>Still Work In Progress ⚡️</p>
