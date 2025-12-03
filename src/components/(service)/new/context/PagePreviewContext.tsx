67cf8555b5aec2fc3943f708fe13dd78055b28ec
olhkyle <itskkyle@gmail.com>
2025-12-03 16:11:37 +0900

    feat : add PagePreview on `PagePreviewContext`

--- src/components/(service)/new/context/PagePreviewContext.tsx
+++ src/components/(service)/new/context/PagePreviewContext.tsx
@@ -1,8 +1,10 @@
 'use client';
 
+import { Page } from 'react-pdf';
 import React from 'react';
-import { SquareMousePointer } from 'lucide-react';
+import { Asterisk, SquareMousePointer } from 'lucide-react';
 import {
+	AnimateSpinner,
 	Button,
 	Dialog,
 	DialogContent,
@@ -18,7 +20,7 @@ import {
 	DrawerTrigger,
 	PageItem,
 } from '@/components';
-import { useMediaQuery } from '@/hooks';
+import { useMediaQuery, useResizableObserver } from '@/hooks';
 import { screenSize } from '@/constant';
 
 interface PagePreviewContextProps {
@@ -27,30 +29,36 @@ interface PagePreviewContextProps {
 	toggle: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-function TriggerButton({ isMobile, ...props }: { isMobile: boolean }) {
+function TriggerButton({ isSMDown, ...props }: { isSMDown: boolean }) {
 	return (
-		<Button type="button" size="icon-sm" variant="ghost" className={`px-${isMobile ? 'auto' : '4'}`} {...props}>
+		<Button type="button" size="icon-sm" variant="ghost" className={`px-${isSMDown ? 'auto' : '4'}`} {...props}>
 			<SquareMousePointer className="text-gray-500" />
 		</Button>
 	);
 }
 
 export default function PagePreviewContext({ page, isOpen, toggle }: PagePreviewContextProps) {
-	const isMobile = useMediaQuery(screenSize.MAX_SM);
+	const [isXSDown, isSMDown] = [useMediaQuery(screenSize.MAX_XS), useMediaQuery(screenSize.MAX_SM)];
+
+	const { containerRef, containerWidth } = useResizableObserver<HTMLDivElement>({
+		initialWidth: typeof window !== 'undefined' && isXSDown ? 320 : window.innerWidth * 0.5,
+	});
+
 	const title = `Page ${page.order} Preview`;
-	const description = `﹡ ${page.id.split('-page-')[0]}.pdf`;
+	const description = `${page.id.split('.pdf')[0]}.pdf`;
 
 	return (
 		<>
-			{isMobile ? (
+			{isSMDown ? (
 				<Drawer open={isOpen} onOpenChange={toggle}>
 					<DrawerTrigger asChild>
-						<TriggerButton isMobile={isMobile} />
+						<TriggerButton isSMDown={isSMDown} />
 					</DrawerTrigger>
 					<DrawerContent>
-						<DrawerHeader className="p-3 text-left">
-							<DrawerTitle className="text-lg">{title}</DrawerTitle>
-							<DrawerDescription className="inline-block py-1.5 px-3 w-auto bg-light text-gray-500 text-sm text-start rounded-md">
+						<DrawerHeader className="p-3">
+							<DrawerTitle className="text-lg text-start">{title}</DrawerTitle>
+							<DrawerDescription className="inline-flex items-center gap-1.5 p-1.5 w-auto bg-gray-200 text-gray-500 text-xs border border-gray-300 rounded-md">
+								<Asterisk size={12} />
 								{description}
 							</DrawerDescription>
 						</DrawerHeader>
@@ -59,15 +67,28 @@ export default function PagePreviewContext({ page, isOpen, toggle }: PagePreview
 			) : (
 				<Dialog open={isOpen} onOpenChange={toggle}>
 					<DialogTrigger asChild>
-						<TriggerButton isMobile={isMobile} />
+						<TriggerButton isSMDown={isSMDown} />
 					</DialogTrigger>
-					<DialogContent className="">
+					<DialogContent ref={containerRef} className="">
 						<DialogHeader>
 							<DialogTitle className="text-lg">{title}</DialogTitle>
-							<DialogDescription className="inline-block py-1.5 px-3 w-auto bg-light text-gray-500 text-sm text-start rounded-md">
+							<DialogDescription className="inline-flex items-center gap-1.5 p-1.5 w-auto bg-gray-200 text-gray-500 text-xs border border-gray-300 rounded-md">
+								<Asterisk size={12} />
 								{description}
 							</DialogDescription>
 						</DialogHeader>
+						<Page
+							devicePixelRatio={2.5}
+							loading={
+								<div className="ui-flex-center w-full h-full bg-light rounded-lg">
+									<AnimateSpinner size={18} />
+								</div>
+							}
+							width={containerWidth}
+							renderTextLayer={false}
+							renderAnnotationLayer={false}
+							className="ui-flex-center w-full border border-gray-200"
+						/>
 					</DialogContent>
 				</Dialog>
 			)}
