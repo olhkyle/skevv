4b66fcece22c254a5f4ee2c848e489d1d06efca6
olhkyle <itskkyle@gmail.com>
2025-12-23 16:04:15 +0900

    feat : add specific meta data with favicons

---
+++ config.ts
@@ -0,0 +1,15 @@
+// google Analytics를 활용한 방문자 수 체크를 위한 config
+// Config of Visitor Check on GA4
+
+export const SiteConfig = {
+	url: 'https://skevv.vercel.app',
+	pathPrefix: '/',
+	title: {
+		default: 'Skevv',
+		HOME: 'Skevv',
+		NEW: 'Skevv | New Merge',
+		DOCUMENTS: 'Skevv | Documents',
+	},
+	subtitle: 'This is a lightweight pdf editor app',
+	copyright: 'Skevv © All rights reserved.',
+};
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from 'next';
 import './globals.css';
-import { Toaster } from '@/components';
-
 import { Inter } from 'next/font/google';
+import { Toaster } from '@/components';
+import { SiteConfig } from '../../config';
 
 const inter = Inter({
 	weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
@@ -13,8 +13,25 @@ const inter = Inter({
 });
 
 export const metadata: Metadata = {
-	title: 'SKEVV',
-	description: 'A lightweight pdf editor',
+	title: {
+		template: `%s - ${SiteConfig.title.default}`,
+		default: SiteConfig.title.default,
+	},
+	description: SiteConfig.subtitle,
+	openGraph: {
+		title: SiteConfig.title.default,
+		description: SiteConfig.subtitle,
+		siteName: 'Skevv',
+		locale: 'ko_KR',
+		type: 'website',
+		url: SiteConfig.url,
+	},
+	// verification: {
+	//   google: process.env.NEXT_PUBLIC_GOOGLE_SEARCH_CONSOLE_ID,
+	//   other: {
+	//     "naver-site-verification": "d40d03ef9270913b96ad108a57d72246fdfc73e8",
+	//   },
+	// },
 };
 
 export default function RootLayout({
@@ -24,6 +41,18 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en" className={`${inter.variable}`}>
+			<head>
+				<link rel="icon" href="/favicon.ico" sizes="any" />
+				<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
+				<link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+				<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+				<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+				<link rel="manifest" href="/site.webmanifest" />
+				<meta name="apple-mobile-web-app-title" content="Skevv" />
+				<meta name="msapplication-TileColor" content="ffffff" />
+				<meta name="theme-color" content="#ffffff" />
+				<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1, user-scalable=0" />
+			</head>
 			<body className="font-inter antialiased bg-white">
 				{children}
 				<Toaster />
