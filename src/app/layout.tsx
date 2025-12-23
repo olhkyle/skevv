bbfb29ff3eda42ccebb7d4a5667a05993f60e427
olhkyle <itskkyle@gmail.com>
2025-12-23 16:36:31 +0900

    refactor : move `config.ts` position

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import './globals.css';
 import { Inter } from 'next/font/google';
 import { Toaster } from '@/components';
-import { SiteConfig } from '../../config';
+import { SiteConfig } from './config';
 
 const inter = Inter({
 	weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
